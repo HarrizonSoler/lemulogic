a6b39dda8103a6f9aa194fff62966e89bb4ae630
Harrizon <harryalex0281@hotmail.com>
2026-03-05 16:49:06 -0500

    tool: add nix environment

---
+++ nix/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "godot dev environment flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        godotPackages_4_3.godot
+      ];
+    };
+  };
+}
